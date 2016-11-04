0d1439658be8bf09bfab0d9858f090f1b10506c5
nick11roberts <n3robert@ucsd.edu>
2016-11-04 15:42:12 -0700

    Added routes for twiml.

---
+++ routes.py
@@ -0,0 +1,29 @@
+from flask import Flask, request
+from twilio import twiml
+
+app = Flask(__name__)
+
+@app.route('/twimlPhonebuzz', methods=['GET', 'POST'])
+def twimlPhonebuzz():
+    if request.method == 'POST':
+        n = int(request.form['fizzbuzznumber'])
+        fizzbuzz = ""
+        for i in range (1, n + 1):
+            if i % 5 ==0 and i % 3 == 0:
+                fizzbuzz += "Fizz Buzz"
+            elif i % 3 == 0:
+                fizzbuzz += "Fizz"
+            elif i % 5 == 0:
+                fizzbuzz += "Buzz"
+            else:
+                fizzbuzz += str(i)
+            if (i + 1) <= n:
+                fizzbuzz += ", "
+        resp = twiml.Response()
+        resp.message(fizzbuzz)
+        return str(resp)
+
+    else if request.method == 'GET':
+        resp = twiml.Response()
+        resp.message("Please enter a number")
+        return str(resp)
